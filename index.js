1f4d5fd0bbf7fd8e8c7ce88deb15e45d4cb4fab8
Ivan Iankovskyi <ivaniankovskyi@XPUAKYIW689402.Dlink>
2018-03-20 01:24:32 +0200

    add log

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'build')))
-  // .set('views', path.join(__dirname, 'views'))
-  // .set('view engine', 'ejs')
+  .use(express.static(path.join(__dirname, 'build/static')))
   .get('/', (req, res) => res.sendFile(path.join(__dirname, 'build/index.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  console.log(path.join(__dirname, 'build/index.html'))
