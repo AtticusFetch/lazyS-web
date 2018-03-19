bcdf009a11f23bab4201080e568c7a02555c2144
Ivan Iankovskyi <ivaniankovskyi@XPUAKYIW689402.Dlink>
2018-03-20 00:55:25 +0200

    fix resp

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const home = require('./build/index.html')
 
 express()
-  .use(express.static(path.join(__dirname, 'build/static')))
+  .use(express.static(path.join(__dirname, 'build')))
   // .set('views', path.join(__dirname, 'views'))
   // .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('build/index.html'))
+  .get('/', (req, res) => res.send(home))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
