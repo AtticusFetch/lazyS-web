cab4df4ebf6b34a157404972ff5ac16557aa09af
Ivan Iankovskyi <ivaniankovskyi@XPUAKYIW689402.Dlink>
2018-03-20 01:37:54 +0200

    clean up

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const express = require('express')
 const path = require('path')
-const fs = require('fs')
 const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'build/static')))
   .get('/', (req, res) => res.sendFile(path.join(__dirname, 'build/index.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-  fs.readdir(__dirname, function(err, items) {
-    for (var i=0; i<items.length; i++) {
-        console.log(items[i]);
-    }
-});
