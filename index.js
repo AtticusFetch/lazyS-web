8225b9526d2bb818034e3ab1fd98e8b32f94d223
Ivan Iankovskyi <ivaniankovskyi@XPUAKYIW689402.Dlink>
2018-03-20 02:32:32 +0200

    fiel upload

--- index.js
+++ index.js
@@ -2,19 +2,14 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs');
 const cloudinary = require('cloudinary')
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'build')))
-  .use(express.bodyParser({uploadDir:'/tmp' }))
   .get('/', (req, res) => res.sendFile(path.join(__dirname, 'build/index.html')))
-  .post('/upload', (req, res) => {
-    console.log('request', req);
-    const tempPath = req.files.file.path;
-    const targetPath = path.resolve('./uploads/image.png');
-    fs.rename(tempPath, targetPath, err => {
-        if (err) throw err;
-        console.log("Upload completed!");
-    });
+  .post('/upload', upload.single('image'), (req, res) => {
+    console.log(req);
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
