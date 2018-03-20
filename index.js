66595af64303db0b0f53e6b58f5e96589e41c583
Ivan Iankovskyi <ivaniankovskyi@XPUAKYIW689402.Dlink>
2018-03-20 02:21:35 +0200

    file upload

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs');
+const cloudinary = require('cloudinary')
 const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'build')))
+  .use(express.bodyParser({uploadDir:'/tmp' }))
   .get('/', (req, res) => res.sendFile(path.join(__dirname, 'build/index.html')))
+  .post('/upload', (req, res) => {
+    console.log('request', req);
+    const tempPath = req.files.file.path;
+    const targetPath = path.resolve('./uploads/image.png');
+    fs.rename(tempPath, targetPath, err => {
+        if (err) throw err;
+        console.log("Upload completed!");
+    });
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- src/App.js
+++ src/App.js
@@ -3,16 +3,76 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    title: '',
+    descrition: '',
+    file: '',
+  };
+
+  _onTitleChange = e => {
+    this.setState({ title: e.target.value });
+  };
+  _onDescriptionChange = e => {
+    this.setState({ descrition: e.target.value });
+  };
+  _onFileChange = e => {
+    let reader = new FileReader();
+    let file = e.target.files[0];
+    reader.onloadend = () => {
+      this.setState({
+        file,
+        imagePreviewUrl: reader.result
+      });
+      console.log(reader.result);
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
+  _onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            Fill data
+          </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form
+          method="post"
+          enctype="multipart/form-data"
+          action="/upload"
+          className="form"
+          onSubmit={this._onSubmit}
+        >
+          <label className="inputWrapper">
+            Item title
+            <input 
+              onChange={this._onTitleChange}
+              className="input"
+              type="text"
+              placeholder="title"
+            />
+          </label>
+          <label>
+            Description
+            <textarea
+              onChange={this._onDescriptionChange}
+            />
+          </label>
+          <label>
+            Image
+            <input
+              onChange={this._onFileChange}
+              type="file"
+            />
+          </label>
+          <input
+            type="submit"
+          />
+        </form>
       </div>
     );
   }
